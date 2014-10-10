78057ff36e95d31cad4a6ce7832f7dde351be8c0
Manu343726 <Manu343726@gmail.com>
2014-10-10 16:01:21 +0200

    Upgrade to 1.8.4

--- checksums.sh
+++ checksums.sh
@@ -3,9 +3,9 @@
 version=$1
 version_label=${version//./_}
 arch=$2
-declare -A package_prefix=(["32"]="ubuntu32"
-                           ["64"]="ubuntu64"
-                           ["pi"]="rpi")
+declare -A package_prefix=(["32"]="ubuntu-32"
+                           ["64"]="ubuntu-64"
+                           ["pi"]="debian-armv6")
 
 package="bii-${package_prefix[$arch]}_${version_label}.deb"
 url="https://s3.amazonaws.com/biibinaries/release/${version}/${package}"
